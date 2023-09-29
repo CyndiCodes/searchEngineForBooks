88950e81720606da7b84daff4211c40827a95262
CyndiCodes <huertascyn@gmail.com>
2023-09-29 16:21:19 -0400

    components on client side updated for loginformjsx and navbarjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,15 @@
 import './App.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` navbar
-const authLink = setContext((_, { navbar }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the navbar to the context so httpLink can read them
-  return {
-    navbar: {
-      ...navbar,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// Uncomment import statement below after building queries and mutations
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Navbar />
-        <div className="container">
-          <Outlet />
-        </div>
-        <Footer />
-      </div>
-    </ApolloProvider>
+    <div className="flex-column justify-center align-center min-100-vh bg-primary">
+      <div>hello</div>
+      <Outlet />
+    </div>
   );
 }
 
