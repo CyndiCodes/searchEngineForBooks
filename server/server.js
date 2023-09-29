45baa6160f1ce24ee98742793036a4cc9edd3202
CyndiCodes <huertascyn@gmail.com>
2023-09-29 10:25:58 -0400

    server.js updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const {ApolloServer} = require ('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const {ApolloServer} = require ('@apollo/server')
 // must delete after and queries and mutations setup 
-const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
 const { typeDefs, resolvers } = require('./schemas');
@@ -15,17 +15,102 @@ const server = new ApolloServer({
 });
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  // Uncomment the following code once you have built the queries and mutations in the client folder
+  app.use('/graphql', expressMiddleware(server));
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  // Comment out this code once you have built out queries and mutations in the client folder
+  app.use(routes);
 
-app.use(routes);
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+    // Uncomment this code once you have built out queries and mutations in the client folder
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } // closes if (process.env.NODE_ENV === 'production') condition
 
+// Uncomment this code once you have built out queries and mutations in the client folder
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+// Comment out this code once you have built out queries and mutations in the client folder
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+// });
+
+// Uncomment the following code once you have built the queries and mutations in the client folder
+startApolloServer();
+
+// const express = require('express');
+// // Uncomment the following code once you have built the queries and mutations in the client folder
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const path = require('path');
+
+// // Uncomment the following code once you have built the queries and mutations in the client folder
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// // Comment out this code once you have built out queries and mutations in the client folder
+// const routes = require('./routes');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// // Uncomment the following code once you have built the queries and mutations in the client folder
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// // Uncomment the following code once you have built the queries and mutations in the client folder
+// const startApolloServer = async () => {
+//   await server.start();
+  
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use(express.json());
+  
+//   // Uncomment the following code once you have built the queries and mutations in the client folder
+//   app.use('/graphql', expressMiddleware(server));
+
+//   // Comment out this code once you have built out queries and mutations in the client folder
+//   app.use(routes);
+
+//   // if we're in production, serve client/dist as static assets
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//     // Uncomment this code once you have built out queries and mutations in the client folder
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//     });
+//   } // closes if (process.env.NODE_ENV === 'production') condition
+
+// // Uncomment this code once you have built out queries and mutations in the client folder
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// // Comment out this code once you have built out queries and mutations in the client folder
+// // db.once('open', () => {
+// //   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+// // });
+
+// // Uncomment the following code once you have built the queries and mutations in the client folder
+// startApolloServer();
