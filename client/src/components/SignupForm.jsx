be3cb488bb41199aca9e360418a616b198f46c26
CyndiCodes <huertascyn@gmail.com>
2023-10-01 22:30:13 -0400

    saving for the day, able to login and create user on the front end, searh book works, save book is going to local storage but not being set on page

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -36,20 +36,15 @@ const SignupForm = () => {
     }
 
     try {
-      // const response = await createUser(userFormData);
+     
       const {data}= await createUser({
         variables:{...userFormData}
       })
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
+      
       Auth.login(data);
     } catch (err) {
       console.error(err);
-      // setShowAlert(true);
+  
     }
 
     setUserFormData({
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,19 +9,13 @@ import {
 } from 'react-bootstrap';
 import {useMutation} from '@apollo/client';
 import Auth from '../utils/auth';
-// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  // const [saveBook, {error}] = useMutation(SAVE_BOOK)
+  const [saveBook, {error}] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -29,7 +23,6 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -74,9 +67,9 @@ const SearchBooks = () => {
     }
 
     try {
-    //  const {data} = await saveBook({
-    //   variable: {bookData:{...bookToSave}}
-    //  })
+     const {data} = await saveBook({
+      variable: {bookData:{...bookToSave}}
+     })
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
